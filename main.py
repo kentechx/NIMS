d53c992737522fa5531f0d3ba07dff4d14377d61
Kentechx <kd_shen@163.com>
2022-02-21 03:25:18 +0800

    update

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import networkx as nx
 import igraph
 from cdlib import algorithms
 import pyreadr
+import pandas as pd
 
 
-g = nx.karate_club_graph()
-a = g.community_spinglass()
-coms = algorithms.louvain(g, weight='weight', resolution=1., randomize=False)
+a = pd.read_excel("our_data.xls")
+pass
