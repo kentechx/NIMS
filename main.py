064429b55106e432f3dcd3baef40aad8777dd2c2
Kentechx <kd_shen@163.com>
2022-02-24 03:00:58 +0800

    update

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import networkx as nx
+import numpy as np
 import igraph
 from cdlib import algorithms
 import pyreadr
 import pandas as pd
 
-igraph.Graph()
+def read_graph(file) ->igraph.Graph:
+    return igraph.read(file)
+
+g = read_graph("a.lgl")
+m = []
+names = g.vs['name']
+for i in range(4):
+    clusters = g.community_spinglass().membership
+    clusters = np.array(clusters)
+    for i in np.unique(clusters):
+        idx = np.where(clusters==i)[0]
+        m.append([names[k] for k in idx])
+
+pass
 
